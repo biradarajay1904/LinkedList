b8b814b1e4e5732d01f4535de50d3d26846a389b
biradarajay1904 <biradarajay04@gmail.com>
2022-07-28 16:47:47 +0530

    added: adding elements

---
+++ LinkedListMain.java
@@ -0,0 +1,14 @@
+package com.LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+
+        linkedList.display();
+
+
+    }
+}
