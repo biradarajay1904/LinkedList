1a24d018ca77bacb406045a462e0a07a1762c32c
biradarajay1904 <biradarajay04@gmail.com>
2022-07-28 16:53:04 +0530

    added: adding linkedlist

--- LinkedList.java
+++ LinkedList.java
@@ -6,27 +6,15 @@ package com.LinkedList;
 */
 
 public class LinkedList<T> {
-	 Node<T> head;
-	    Node<T> tail;
+	Node<T> head;
+    Node<T> tail;
 
-	    void push(T data){
-	        Node<T> node = new Node<>(data);
-	        if(head == null){
-	            head = node;
-	            tail = node;
-	        }
-	        else{
-	            node.next = head;
-	            head = node;
-	        }
-	    }
-
-	    void display(){
-	        Node<T> temp = head;
-	        while (temp != null){
-	            System.out.print(temp.data+ " ");
-	            temp = temp.next;
-	        }
-	    }
+    void display(){
+        Node<T> temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
 
 }
