bf14f85d92d916a3bb7c40b6ee221a1d3b741426
biradarajay1904 <biradarajay04@gmail.com>
2022-07-31 20:29:37 +0530

    added: inserting elements

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -4,25 +4,16 @@ package com.LinkedList;
 
 public class LinkedListMain {
 
-	    public static void main(String[] args) {
-	    	
-	        LinkedList<Integer> linkedList = new LinkedList<>();
-	        linkedList.push(70);
-	        linkedList.push(30);
-	        linkedList.push(56);
+	 
+	    	public static void main(String[] args) {
+	            LinkedList<Integer> linkedList = new LinkedList<>();
+	            linkedList.push(70);
+	            linkedList.push(56);
+
+	            linkedList.insert(30);
+
+	            linkedList.display();
 
 
-	        linkedList.print();
-	        System.out.println();
-
-	        LinkedList<Integer> linkedList1 = new LinkedList<>();
-
-	        linkedList1.add(70);
-	        linkedList1.add(30);
-	        linkedList1.add(56);
-
-	        linkedList1.print();
-
-
+	        }
 	    }
-	}
