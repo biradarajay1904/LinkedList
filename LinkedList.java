2dbeede989365390e9a5e9add076dc992710d93f
biradarajay1904 <biradarajay04@gmail.com>
2022-07-31 20:51:32 +0530

    added: searching  elements

--- LinkedList.java
+++ LinkedList.java
@@ -4,61 +4,73 @@ package com.LinkedList;
 
 
 */
-
 public class LinkedList<T> {
-	 Node<T> head;
-	    Node<T> tail;
+    Node<T> head;
+    Node<T> tail;
 
-	    void push(T data){
-	        Node<T> node = new Node<>(data);
-	        if(head == null){
-	            head = node;
-	            tail = node;
-	        }
-	        else{
-	            node.next = head;
-	            head = node;
-	        }
-	    }
+    void push(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head = node;
+        }
+    }
 
-	    void add(T data){
-	        Node<T> node = new Node<>(data);
-	        if(head == null){
-	            head = node;
-	            tail = node;
-	        }
-	        else{
-	            tail.next = node;
-	            tail = node;
-	        }
-	    }
+    public Node<T> search(T searchData) {
+        Node<T> temp = head;
+        while(temp != null){
+            if(temp.data.equals(searchData))
+                return temp;
+            temp = temp.next;
+        }
+        return null;
+    }
 
-	    void display(){
-	        Node<T> temp = head;
-	        while (temp != null){
-	            System.out.print(temp.data+ " ");
-	            temp = temp.next;
-	        }
-	    }
+    void add(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.next = node;
+            tail = node;
+        }
+    }
 
-	    public void insert(T data) {
-	        Node<T> node = new Node<>(data);
-	        head.next = node;
-	        node.next = tail;
-	    }
-	    public T pop() {
-	        T deletedElement = head.data;
-	        head = head.next;
-	        return deletedElement;
-	    }
-	    public T popLast() {
-	        T deletedElement = tail.data;
-	        Node<T> temp = head;
-	        while(temp.next != tail){
-	            temp = temp.next;
-	        }
-	        temp.next = null;
-	        temp = tail;
-	        return  deletedElement;
-	    }
-	}
+    void display(){
+        Node<T> temp = head;
+        while (temp != null){
+            System.out.print(temp.data+ " ");
+            temp = temp.next;
+        }
+    }
+
+    public void insert(T data) {
+        Node<T> node = new Node<>(data);
+        head.next = node;
+        node.next = tail;
+    }
+
+    public T pop() {
+        T deletedElement = head.data;
+        head = head.next;
+        return deletedElement;
+    }
+
+    public T popLast() {
+        T deletedElement = tail.data;
+        Node<T> temp = head;
+        while(temp.next != tail){
+            temp = temp.next;
+        }
+        temp.next = null;
+        temp = tail;
+        return  deletedElement;
+
+    }
+}
